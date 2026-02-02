6e52b690015ffe75ab63f5ddc18ffdfeb8585fed
Ishacodeuser <inegi9336@gmail.com>
2026-02-02 19:43:15 +0530

    Refactor isUgly method and fix main method call

--- UglyNumber.java
+++ UglyNumber.java
@@ -18,34 +18,4 @@ public class uglyNumber{
             } else {
                 return false;
             }
-        }package JavaProgram;
-
-public class uglyNumber {
-    public static void main(String[] args) {
-        int n =6;
-        System.out.println(isUgly(n));
-    }
-    public static boolean isUgly(int n) {
-        if (n <= 0) {
-            return false;
         }
-
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 5 == 0) {
-                n /= 5;
-            } else {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-        return true;
-    }
-}
