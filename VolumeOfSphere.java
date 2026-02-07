76fdb3dfb9e0e20bbb09a6ce17aabcc6553f189a
Isha <inegi9336@gmail.com>
2026-02-07 13:08:57 +0530

    Add files via upload

---
+++ VolumeOfSphere.java
@@ -0,0 +1,14 @@
+package JavaProgram;
+import java.util.Scanner;
+
+public class SphereVolume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of the sphere: ");
+        double radius = sc.nextDouble();
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        System.out.println("Volume of the sphere is: " + volume);
+
+        sc.close();
+    }
+}
