273aa5eb1b74620c9ae7c37c6b75f49d48f17834
Isha <inegi9336@gmail.com>
2026-02-17 19:41:34 +0530

    Add files via upload

---
+++ Valid_anagram.java
@@ -0,0 +1,26 @@
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int num : count) {
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        ValidAnagram solution = new ValidAnagram();
+
+        System.out.println(solution.isAnagram("anagram", "nagaram")); 
+        System.out.println(solution.isAnagram("rat", "car"));         
+    }
+}
