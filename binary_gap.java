ba5794d1d8a56e54d2a0cf019923e070942b4d11
Isha <inegi9336@gmail.com>
2026-02-22 15:25:34 +0530

    Add files via upload

---
+++ binary_gap.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int longestDistanceBetweenOnes(int n) {
+        String binary = Integer.toBinaryString(n);
+        int lastIndex = -1;
+        int maxDistance = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '1') {
+                if (lastIndex != -1) {
+                    int distance = i - lastIndex;
+                    maxDistance = Math.max(maxDistance, distance);
+                }
+                lastIndex = i;
+            }
+        }
+
+        return maxDistance;
+    }
+}
