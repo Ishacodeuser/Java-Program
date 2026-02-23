5418af83ae6529fe2e540d0814e12d01ca05dc93
Isha <inegi9336@gmail.com>
2026-02-23 21:51:26 +0530

    Add files via upload

---
+++ Fibonacci_number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = a + b; 
+            a = b;         
+            b = c;         
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.fibonacci(0));  
+        System.out.println(sol.fibonacci(1));  
+        System.out.println(sol.fibonacci(5));  
+        System.out.println(sol.fibonacci(10)); 
+    }
+}
