bf9731a08c827e1208acb9366b1daeac101f261a
Isha <inegi9336@gmail.com>
2026-03-09 16:00:29 +0530

    Add files via upload

---
+++ Find_all_possible_stable_binary_Arrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    static final int MOD = 1000000007;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        long[][][] dp = new long[zero + 1][one + 1][2];
+
+        for (int i = 1; i <= Math.min(zero, limit); i++) dp[i][0][0] = 1;
+        for (int j = 1; j <= Math.min(one, limit); j++) dp[0][j][1] = 1;
+
+        for (int z = 1; z <= zero; z++) {
+            for (int o = 1; o <= one; o++) {
+                long val0 = (dp[z-1][o][0] + dp[z-1][o][1]) % MOD;
+                if (z > limit) val0 = (val0 - dp[z-limit-1][o][1] + MOD) % MOD;
+                dp[z][o][0] = val0;
+
+                long val1 = (dp[z][o-1][0] + dp[z][o-1][1]) % MOD;
+                if (o > limit) val1 = (val1 - dp[z][o-limit-1][0] + MOD) % MOD;
+                dp[z][o][1] = val1;
+            }
+        }
+
+        return (int)((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+    }
+}
